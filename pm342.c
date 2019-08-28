f52337d9d0bd75c31fa130fb7c6d8d88fc4fea7d
balika011 <balika011@gmail.com>
2019-08-28 18:46:05 +0200

    Add FT4232H and TCA9539 definitions to pm342.c

--- pm342.c
+++ pm342.c
@@ -1,9 +1,44 @@
 #include <unistd.h>
 #include "pm342.h"
 
+#define FT4232H_VID 0x0403
+#define FT4232H_PID 0x6011
+
+#define TCA9539_A0_L_A1_L_ADDR 0x74
+#define TCA9539_A0_L_A1_H_ADDR 0x75
+#define TCA9539_A0_H_A1_L_ADDR 0x76
+#define TCA9539_A0_H_A1_H_ADDR 0x77
+
+#define TCA9539_INPUT_PORT_0_REG 0x00
+#define TCA9539_INPUT_PORT_1_REG 0x01
+#define TCA9539_OUTPUT_PORT_0_REG 0x02
+#define TCA9539_OUTPUT_PORT_1_REG 0x03
+#define TCA9539_POLERITY_INVERSION_PORT_0_REG 0x04
+#define TCA9539_POLERITY_INVERSION_PORT_1_REG 0x05
+#define TCA9539_CONFIGURATION_PORT_0_REG 0x06
+#define TCA9539_CONFIGURATION_PORT_1_REG 0x07
+
+#define TCA9539_P00_FLAG (1 << 0)
+#define TCA9539_P01_FLAG (1 << 1)
+#define TCA9539_P02_FLAG (1 << 2)
+#define TCA9539_P03_FLAG (1 << 3)
+#define TCA9539_P04_FLAG (1 << 4)
+#define TCA9539_P05_FLAG (1 << 5)
+#define TCA9539_P06_FLAG (1 << 6)
+#define TCA9539_P07_FLAG (1 << 7)
+
+#define TCA9539_P10_FLAG (1 << 0)
+#define TCA9539_P11_FLAG (1 << 1)
+#define TCA9539_P12_FLAG (1 << 2)
+#define TCA9539_P13_FLAG (1 << 3)
+#define TCA9539_P14_FLAG (1 << 4)
+#define TCA9539_P15_FLAG (1 << 5)
+#define TCA9539_P16_FLAG (1 << 6)
+#define TCA9539_P17_FLAG (1 << 7)
+
 int pm342_get_devices(const char ***descriptions, const char ***serials, unsigned int *num_devices)
 {
-	return i2c_get_devices(0x403, 0x6011, descriptions, serials, num_devices);
+	return i2c_get_devices(FT4232H_VID, FT4232H_PID, descriptions, serials, num_devices);
 }
 
 pm342_context *pm342_new_desc(int vid, int pid, const char *description, const char *serial, unsigned int index)
@@ -29,7 +64,7 @@ pm342_context *pm342_new_desc(int vid, int pid, const char *description, const c
 
 pm342_context *pm342_new(char *product, char *serial, unsigned int index)
 {
-	return pm342_new_desc(0x403, 0x6011, product, serial, index);
+	return pm342_new_desc(FT4232H_VID, FT4232H_PID, product, serial, index);
 }
 
 void pm342_free(pm342_context *pm342)
@@ -51,52 +86,52 @@ int pm342_toggle_keys(pm342_context *pm342, PM342PhidgetKeys keys, int state, in
 
 int pm342_set_keys(pm342_context *pm342, PM342PhidgetKeys keys, int state)
 {
-	uint8_t configPort1;
-	uint8_t outputPort1;
+	uint8_t configPort;
+	uint8_t outputPort;
 
 	if (keys & PM342PK_RESET)
 	{
-		if (i2c_read(pm342->i2c, 0x74, 6, &configPort1))
+		if (i2c_read(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_CONFIGURATION_PORT_0_REG, &configPort))
 			return 1;
 
-		if (i2c_read(pm342->i2c, 0x74, 2, &outputPort1))
+		if (i2c_read(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_OUTPUT_PORT_0_REG, &outputPort))
 			return 1;
 
 		if (state & PM342PK_RESET)
 		{
-			configPort1 &= ~8;
-			outputPort1 &= ~8;
+			configPort &= ~TCA9539_P03_FLAG;
+			outputPort &= ~TCA9539_P03_FLAG;
 		}
 		else
 		{
-			configPort1 |= 8;
-			outputPort1 |= 8;
+			configPort |= TCA9539_P03_FLAG;
+			outputPort |= TCA9539_P03_FLAG;
 		}
 
-		if (i2c_write(pm342->i2c, 0x74, 6, configPort1))
+		if (i2c_write(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_CONFIGURATION_PORT_0_REG, configPort))
 			return 1;
-		if (i2c_write(pm342->i2c, 0x74, 2, outputPort1))
+		if (i2c_write(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_OUTPUT_PORT_0_REG, outputPort))
 			return 1;
 	}
 
 	if (keys & (PM342PK_ONKEY | PM342PK_FORCE_RECOVERY))
 	{
-		if (i2c_read(pm342->i2c, 0x74, 7, &configPort1))
+		if (i2c_read(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_CONFIGURATION_PORT_1_REG, &configPort))
 			return 1;
-		if (i2c_read(pm342->i2c, 0x74, 3, &outputPort1))
+		if (i2c_read(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_OUTPUT_PORT_1_REG, &outputPort))
 			return 1;
 
 		if (keys & PM342PK_ONKEY)
 		{
 			if (state & PM342PK_ONKEY)
 			{
-				configPort1 &= ~0x10;
-				outputPort1 &= ~0x10;
+				configPort &= ~TCA9539_P14_FLAG;
+				outputPort &= ~TCA9539_P14_FLAG;
 			}
 			else
 			{
-				configPort1 |= 0x10;
-				outputPort1 |= 0x10;
+				configPort |= TCA9539_P14_FLAG;
+				outputPort |= TCA9539_P14_FLAG;
 			}
 		}
 
@@ -104,19 +139,19 @@ int pm342_set_keys(pm342_context *pm342, PM342PhidgetKeys keys, int state)
 		{
 			if (state & PM342PK_FORCE_RECOVERY)
 			{
-				configPort1 &= ~8;
-				outputPort1 &= ~8;
+				configPort &= ~TCA9539_P13_FLAG;
+				outputPort &= ~TCA9539_P13_FLAG;
 			}
 			else
 			{
-				configPort1 |= 8;
-				outputPort1 |= 8;
+				configPort |= TCA9539_P13_FLAG;
+				outputPort |= TCA9539_P13_FLAG;
 			}
 		}
 
-		if (i2c_write(pm342->i2c, 0x74, 7, configPort1))
+		if (i2c_write(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_CONFIGURATION_PORT_1_REG, configPort))
 			return 1;
-		if (i2c_write(pm342->i2c, 0x74, 3, outputPort1))
+		if (i2c_write(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_OUTPUT_PORT_1_REG, outputPort))
 			return 1;
 	}
 
@@ -141,19 +176,18 @@ int pm342_set_keys(pm342_context *pm342, PM342PhidgetKeys keys, int state)
 int pm342_get_status(pm342_context *pm342, int *vdd_core_rail, int *vdd_cpu_rail)
 {
 	uint8_t configPort;
-	if (i2c_read(pm342->i2c, 0x74, 7, &configPort))
+	if (i2c_read(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_CONFIGURATION_PORT_1_REG, &configPort))
 		return 1;
-
-	configPort |= 0xC0;
-	if (i2c_write(pm342->i2c, 0x74, 7, configPort) )
+	configPort |= TCA9539_P16_FLAG | TCA9539_P17_FLAG;
+	if (i2c_write(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_CONFIGURATION_PORT_1_REG, configPort) )
 		return 1;
 
 	uint8_t inputPort;
-	if (i2c_read(pm342->i2c, 0x74, 1, &inputPort))
+	if (i2c_read(pm342->i2c, TCA9539_A0_L_A1_L_ADDR, TCA9539_INPUT_PORT_1_REG, &inputPort))
 		return 1;
 
-	*vdd_core_rail = (inputPort & 0x40) == 0;
-	*vdd_cpu_rail = ~inputPort >> 7;
+	*vdd_core_rail = ~inputPort & TCA9539_P16_FLAG;
+	*vdd_cpu_rail = ~inputPort & TCA9539_P17_FLAG;
 
 	return 0;
 }
